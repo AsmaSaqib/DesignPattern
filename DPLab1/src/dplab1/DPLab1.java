a0ea8957e1e0a96cbd03c755829ad8a7d8e03e5e
AbubakrKaakar <92436704+AbubakrKaakar@users.noreply.github.com>
2023-09-15 09:41:15 +0500

    DP added

---
+++ DPLab1/src/dplab1/DPLab1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dplab1;
+
+/**
+ *
+ * @author sp20-bse-072
+ */
+public class DPLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
