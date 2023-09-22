8389c5b36fd86759f444da9cf3ee747eeabc4aa1
AbubakrKaakar <92436704+AbubakrKaakar@users.noreply.github.com>
2023-09-22 09:38:59 +0500

    main class changed

--- Lab2/Part 1/Singleton_Pattern/src/singleton_pattern/Test.java
+++ Lab2/Part 1/Singleton_Pattern/src/singleton_pattern/Test.java
@@ -9,13 +9,16 @@ package singleton_pattern;
  *
  * @author sp20-bse-072
  */
-public class Singleton_Pattern {
+public class Test {
 
 
     public static void main(String[] args) {
         // TODO code application logic here
-//        Connection c = Connection.getInstance();
-//		c.getData();
+        //here I tested the first github exmaple.
+        Connection c = Connection.getInstance();
+		c.getData();
+
+       //Here second one is tested.
      Configuration config = Configuration.getInstance();
 		System.out.println(config.getValue("mode"));
 		System.out.println(config.getValue("font-size"));
