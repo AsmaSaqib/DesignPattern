2e991d9aa6bd7dfc1a4ceaa98dbe0f7c41e9e8e0
AbubakrKaakar <92436704+AbubakrKaakar@users.noreply.github.com>
2023-09-22 09:08:39 +0500

    Part 1 added

---
+++ Lab2/Part 1/Singleton_Pattern/src/singleton_pattern/Singleton_Pattern.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singleton_pattern;
+
+/**
+ *
+ * @author sp20-bse-072
+ */
+public class Singleton_Pattern {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Connection c = Connection.getInstance();
+		c.getData();
+
+    }
+    
+}
