5fe31c50684d08b26588f5190a0816f46a8cd00a
AbubakrKaakar <92436704+AbubakrKaakar@users.noreply.github.com>
2023-09-22 09:42:55 +0500

    Observer added

---
+++ Lab2/Part 2/Observer_Pattern/src/observer_pattern/Observer_Pattern.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer_pattern;
+
+/**
+ *
+ * @author sp20-bse-072
+ */
+public class Observer_Pattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
